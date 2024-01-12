e8850ce42c0c295748f281a72d738b74b3927b53
BobaUbisoft17 <aksud2316@gmail.com>
2024-01-12 14:44:09 +0300

    Move default means from config.py to docker-compose.yml

--- chsu_bot/config.py
+++ chsu_bot/config.py
@@ -8,8 +8,8 @@ class Config(BaseSettings):
 
     ADMIN: int
     BOTTOKEN: str
-    RUNTYPE: str = "polling"
-    DBURL: str = "./bot.db"
-    DOMEN: str = "127.0.0.1"
-    HOST: str = "localhost"
-    PORT: int = 8080
+    RUNTYPE: str
+    DBURL: str
+    DOMEN: str
+    HOST: str
+    PORT: int
